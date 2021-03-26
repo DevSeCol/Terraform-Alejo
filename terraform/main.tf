06e22d74a9ffce47adf31f69ec5d32d797837c86
servas1 <vanegas1234@gmail.com>
2021-03-26 05:33:39 -0500

    patch terraform main

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,6 @@ resource "google_compute_instance" "default" {
   name         = "test"
   machine_type = "e2-medium"
   zone         = "us-central1-a"
-  project = "devsecol"
 
   tags = ["foo", "bar"]
 
